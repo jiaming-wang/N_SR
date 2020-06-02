6470b15914f93153fe6dabe9d95ba67d9d844b81
Jiaming-Wang <wjmecho@163.com>
2020-06-02 22:42:53 +0800

    Update rcan.py

--- model/rcan.py
+++ model/rcan.py
@@ -3,7 +3,7 @@
 '''
 @Author: wjm
 @Date: 2020-06-02 21:02:40
-@LastEditTime: 2020-06-02 21:52:00
+@LastEditTime: 2020-06-02 22:42:41
 @Description: batch_size=16, patch_size=48, L1 loss, epoch=1000, lr=1e-4, decay=200
 '''
 import os
@@ -35,13 +35,12 @@ class CALayer(nn.Module):
 ## Residual Channel Attention Block (RCAB)
 class RCAB(nn.Module):
     def __init__(
-        self, n_feat, kernel_size, reduction,
-        bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+        self, n_feat, kernel_size, reduction, act=nn.ReLU(True), res_scale=1):
 
         super(RCAB, self).__init__()
         modules_body = []
         for i in range(2):
-            modules_body.append(ConvBlock(n_feat, n_feat, kernel_size, 3, 1, 1, activation=None, norm=None))
+            modules_body.append(ConvBlock(n_feat, n_feat, 3, 1, 1, activation=None, norm=None))
             # if bn: modules_body.append(nn.BatchNorm2d(n_feat))
             if i == 0: modules_body.append(act)
         modules_body.append(CALayer(n_feat, reduction))
@@ -51,7 +50,7 @@ class RCAB(nn.Module):
     def forward(self, x):
         res = self.body(x)
         #res = self.body(x).mul(self.res_scale)
-        res += x
+        res = res + x
         return res
 
 ## Residual Group (RG)
@@ -61,14 +60,14 @@ class ResidualGroup(nn.Module):
         modules_body = []
         modules_body = [
             RCAB(
-                n_feat, kernel_size, reduction, bias=True, bn=False, act=nn.ReLU(True), res_scale=1) \
+                n_feat, kernel_size, reduction, act=nn.ReLU(True), res_scale=1) \
             for _ in range(n_resblocks)]
-        modules_body.append(ConvBlock(n_feat, n_feat, kernel_size, 3, 1, 1, activation=None, norm=None))
+        modules_body.append(ConvBlock(n_feat, n_feat, 3, 1, 1, activation=None, norm=None))
         self.body = nn.Sequential(*modules_body)
 
     def forward(self, x):
         res = self.body(x)
-        res += x
+        res = res + x
         return res
 
 class Net(nn.Module):
@@ -102,7 +101,7 @@ class Net(nn.Module):
         x = self.head(x)
         
         res = self.body(x)
-        res += x
+        res = res + x
         
         x = self.up(res)
         x = self.output_conv(x)
